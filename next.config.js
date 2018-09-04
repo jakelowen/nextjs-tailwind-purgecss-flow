417008ad6c115746eb15cd2c9eada3c870986093
Jake Lowen <jakelowen@gmail.com>
2018-09-03 20:21:13 -0500

    fixing purgecss installation

--- next.config.js
+++ next.config.js
@@ -1,5 +1,53 @@
+/* eslint-disable */
 const withCss = require("@zeit/next-css");
 const withPurgeCss = require("next-purgecss");
+const webpack = require("webpack");
+let glob = require("glob-all");
+const path = require("path");
+
+// for environment variables
+require("dotenv").config({
+  path: process.env.NODE_ENV === "production" ? ".env.production" : ".env"
+});
+
+class TailwindExtractor {
+  static extract(content) {
+    return content.match(/[A-Za-z0-9-_:\/]+/g) || [];
+  }
+}
 
 // with help from https://github.com/lucleray/next-purgecss
-module.exports = withCss(withPurgeCss());
+// and purgecss webpack example from https://gist.github.com/andrewdelprete/d2f44d0c7f120aae1b8bd87cbf0e3bc8
+module.exports = withCss(
+  withPurgeCss({
+    purgeCss: {
+      // Specify the locations of any files you want to scan for class names.
+      paths: glob.sync([
+        path.join(__dirname, "pages/**/*.jsx"),
+        path.join(__dirname, "components/**/*.jsx"),
+        path.join(__dirname, "components/**/*.js")
+      ]),
+      extractors: [
+        {
+          extractor: TailwindExtractor,
+
+          // Specify the file extensions to include when scanning for
+          // class names.
+          extensions: ["jsx", "js"]
+        }
+      ],
+      whitelist: () => ["body", "html"]
+    },
+    // needed for .env stuff
+    webpack: config => {
+      const env = Object.keys(process.env).reduce((acc, curr) => {
+        acc[`process.env.${curr}`] = JSON.stringify(process.env[curr]);
+        return acc;
+      }, {});
+
+      config.plugins.push(new webpack.DefinePlugin(env));
+
+      return config;
+    }
+  })
+);
